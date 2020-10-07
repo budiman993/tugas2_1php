2c97edfd482bc1e9fc4785303c35f9306423322b
budiman993 <71246269+budiman993@users.noreply.github.com>
2020-10-07 12:00:33 +0700

    Add files via upload

---
+++ tugas2_1.php
@@ -0,0 +1,17 @@
+<?php
+$pekerja1 = 12;
+$pekerja2= 24;
+$hari1 = 36;
+$hari2 = ($pekerja1/$pekerja2)*$hari1;
+
+echo $hari1, " Hari = ", $pekerja1, " Pekerja";
+echo "<br>X Hari = ", $pekerja2, " Pekerja";
+echo "<br>X = ...?";
+
+
+
+echo "<br><br>Jawaban : ", $hari2, " Hari";
+
+
+
+?>
